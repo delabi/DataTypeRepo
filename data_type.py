a22489afb7d1178649645f30983741447a5bda9d
delabi <bwire.laban@gmail.com>
2017-04-05 00:27:19 +0300

    Code complete, Zero FAILS on tests

--- data_type.py
+++ data_type.py
@@ -1,1 +1,21 @@
-def data_type():
+def data_type(a):
+	if isinstance(a, str):
+		return len(a)
+	if a == None :
+		return 'no value'
+	if isinstance(a, list):
+		if  len(a) > 2:
+			return a[2]
+		return None
+	if isinstance(a, bool):
+		return a
+	if isinstance(a, int):
+		msg = ''
+		if a < 100:
+			msg = 'less than 100'
+		if a > 100:
+			msg = 'more than 100'
+		if a == 100:
+			msg = 'equal to 100'
+		return msg
+
